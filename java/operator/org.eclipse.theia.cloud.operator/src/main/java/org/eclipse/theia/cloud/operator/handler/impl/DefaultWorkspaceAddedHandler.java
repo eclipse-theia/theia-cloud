438fbf79d43aca1ef948e341d7b7b630ea92fe65
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2022-01-26 10:04:11 +0100

    Add authentication
    * currently updating the deployment with the new sidecar container
    restarts the deployment

--- java/operator/org.eclipse.theia.cloud.operator/src/main/java/org/eclipse/theia/cloud/operator/handler/impl/DefaultWorkspaceAddedHandler.java
+++ java/operator/org.eclipse.theia.cloud.operator/src/main/java/org/eclipse/theia/cloud/operator/handler/impl/DefaultWorkspaceAddedHandler.java
@@ -18,7 +18,12 @@ package org.eclipse.theia.cloud.operator.handler.impl;
 
 import static org.eclipse.theia.cloud.operator.util.LogMessageUtil.formatLogMessage;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 
@@ -31,11 +36,18 @@ import org.eclipse.theia.cloud.operator.resource.TemplateSpecResourceList;
 import org.eclipse.theia.cloud.operator.resource.WorkspaceSpec;
 import org.eclipse.theia.cloud.operator.resource.WorkspaceSpecResource;
 import org.eclipse.theia.cloud.operator.util.JavaUtil;
+import org.eclipse.theia.cloud.operator.util.ResourceUtil;
 
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapList;
+import io.fabric8.kubernetes.api.model.Container;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.OwnerReference;
+import io.fabric8.kubernetes.api.model.PodSpec;
 import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.Volume;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentList;
 import io.fabric8.kubernetes.api.model.networking.v1.HTTPIngressPath;
 import io.fabric8.kubernetes.api.model.networking.v1.HTTPIngressRuleValue;
 import io.fabric8.kubernetes.api.model.networking.v1.Ingress;
@@ -45,10 +57,22 @@ import io.fabric8.kubernetes.api.model.networking.v1.IngressServiceBackend;
 import io.fabric8.kubernetes.api.model.networking.v1.ServiceBackendPort;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import io.fabric8.kubernetes.client.dsl.RollableScalableResource;
 
 public class DefaultWorkspaceAddedHandler implements WorkspaceAddedHandler {
 
     private static final Logger LOGGER = LogManager.getLogger(DefaultWorkspaceAddedHandler.class);
+
+    public static final String OAUTH2_PROXY_CONFIGMAP_NAME = "oauth2-proxy-config";
+    public static final String OAUTH2_PROXY_CFG = "oauth2-proxy.cfg";
+
+    protected static final String PLACEHOLDER_CONFIGNAME = "placeholder-configname";
+    protected static final String PLACEHOLDER_NAMESPACE = DefaultTemplateAddedHandler.PLACEHOLDER_NAMESPACE;
+
+    protected static final String TEMPLATE_O_AUTH_SIDECAR_YAML = "/templateOAuthSidecar.yaml";
+    protected static final String TEMPLATE_CONFIGMAP_YAML = "/templateConfigmap.yaml";
 
     @Override
     public boolean handle(DefaultKubernetesClient client, WorkspaceSpecResource workspace, String namespace,
@@ -95,6 +119,20 @@ public class DefaultWorkspaceAddedHandler implements WorkspaceAddedHandler {
 	Optional<Service> serviceToUse = reserveServiceResult.getKey();
 	if (serviceToUse.isEmpty()) {
 	    LOGGER.error(formatLogMessage(correlationId, "No Service for template " + templateID + " found."));
+	    return false;
+	}
+
+	/* Create oauth config map and set hostname */
+	String host = getHost(user, workspaceName, ingress.get());
+	ConfigMap templateConfigMap = client.configMaps().inNamespace(namespace).withName(OAUTH2_PROXY_CONFIGMAP_NAME)
+		.get();
+	Map<String, String> data = new LinkedHashMap<>(templateConfigMap.getData());
+	data.put(OAUTH2_PROXY_CFG, data.get(OAUTH2_PROXY_CFG).replace("https://placeholder", "https://" + host));
+	boolean configMapCreationSuccessful = createAndApplyConfigMap(client, namespace, correlationId,
+		workspaceResourceName, workspaceResourceUID, user, workspaceName, data);
+	if (!configMapCreationSuccessful) {
+	    LOGGER.error(formatLogMessage(correlationId, "Could not create configmap"));
+	    return false;
 	}
 
 	/* get the deployment for the service */
@@ -111,8 +149,8 @@ public class DefaultWorkspaceAddedHandler implements WorkspaceAddedHandler {
 	try {
 	    client.apps().deployments().inNamespace(namespace)
 		    .withName(templateID + DefaultTemplateAddedHandler.DEPLOYMENT_NAME + instance)
-		    .edit(deployment -> addOwnerReferenceToDeployment(correlationId, workspaceResourceName,
-			    workspaceResourceUID, deployment));
+		    .edit(deployment -> adjustDeployment(client, correlationId, workspaceResourceName,
+			    workspaceResourceUID, namespace, user, workspaceName, deployment));
 	} catch (KubernetesClientException e) {
 	    LOGGER.error(formatLogMessage(correlationId, "Error while editing deployment "
 		    + (templateID + DefaultTemplateAddedHandler.DEPLOYMENT_NAME + instance)), e);
@@ -177,11 +215,11 @@ public class DefaultWorkspaceAddedHandler implements WorkspaceAddedHandler {
 	    String workspaceName, Optional<Ingress> ingress, Optional<Service> serviceToUse) {
 	client.network().v1().ingresses().inNamespace(namespace).withName(ingress.get().getMetadata().getName())
 		.edit(ingressToUpdate -> {
-		    IngressRule firstIngressRule = ingressToUpdate.getSpec().getRules().get(0);
+		    String host = getHost(user, workspaceName, ingressToUpdate);
 
 		    IngressRule ingressRule = new IngressRule();
 		    ingressToUpdate.getSpec().getRules().add(ingressRule);
-		    ingressRule.setHost(user + "." + workspaceName + "." + firstIngressRule.getHost());
+		    ingressRule.setHost(host);
 
 		    HTTPIngressRuleValue http = new HTTPIngressRuleValue();
 		    ingressRule.setHttp(http);
@@ -204,6 +242,57 @@ public class DefaultWorkspaceAddedHandler implements WorkspaceAddedHandler {
 
 		    return ingressToUpdate;
 		});
+    }
+
+    protected String getHost(String user, String workspaceName, Ingress ingress) {
+	IngressRule firstIngressRule = ingress.getSpec().getRules().get(0);
+	return user + "." + workspaceName + "." + firstIngressRule.getHost();
+    }
+
+    protected String getConfigMapName(String user, String workspaceName) {
+	return user + "-" + workspaceName + "-oauth2-proxy-config";
+    }
+
+    protected Deployment adjustDeployment(DefaultKubernetesClient client, String correlationId,
+	    String workspaceResourceName, String workspaceResourceUID, String namespace, String user,
+	    String workspaceName, Deployment deployment) {
+
+	deployment = addOwnerReferenceToDeployment(correlationId, workspaceResourceName, workspaceResourceUID,
+		deployment);
+
+	/* create yaml based on template */
+	Map<String, String> replacements = getSidecarReplacements(user, workspaceName);
+	String deploymentYaml;
+	try {
+	    deploymentYaml = ResourceUtil.readResourceAndReplacePlaceholders(DefaultWorkspaceAddedHandler.class,
+		    TEMPLATE_O_AUTH_SIDECAR_YAML, replacements, correlationId);
+	} catch (IOException | URISyntaxException e) {
+	    LOGGER.error(formatLogMessage(correlationId, "Error while adjusting template"), e);
+	    return deployment;
+	}
+
+	try (ByteArrayInputStream inputStream = new ByteArrayInputStream(deploymentYaml.getBytes())) {
+	    /* prepare new configmap */
+	    NonNamespaceOperation<Deployment, DeploymentList, RollableScalableResource<Deployment>> deployments = client
+		    .apps().deployments().inNamespace(namespace);
+	    LOGGER.trace(formatLogMessage(correlationId, "Loading new side car deployment:\n" + deploymentYaml));
+	    Deployment sidecar = deployments.load(inputStream).get();
+
+	    PodSpec sidecarSpec = sidecar.getSpec().getTemplate().getSpec();
+	    PodSpec deploymentSpec = deployment.getSpec().getTemplate().getSpec();
+
+	    Container oAuthContainer = sidecarSpec.getContainers().get(0);
+	    deploymentSpec.getContainers().add(oAuthContainer);
+
+	    List<Volume> volumes = sidecarSpec.getVolumes();
+	    deploymentSpec.getVolumes().addAll(volumes);
+
+	} catch (IOException e) {
+	    LOGGER.error(formatLogMessage(correlationId, "Error with template stream"), e);
+	    return deployment;
+	}
+
+	return deployment;
     }
 
     protected Deployment addOwnerReferenceToDeployment(String correlationId, String workspaceResourceName,
@@ -231,6 +320,54 @@ public class DefaultWorkspaceAddedHandler implements WorkspaceAddedHandler {
 	ownerReference.setName(workspaceResourceName);
 	ownerReference.setUid(workspaceResourceUID);
 	return ownerReference;
+    }
+
+    protected boolean createAndApplyConfigMap(DefaultKubernetesClient client, String namespace, String correlationId,
+	    String workspaceResourceName, String workspaceResourceUID, String user, String workspaceName,
+	    Map<String, String> data) {
+	/* create yaml based on template */
+	Map<String, String> replacements = getConfigMapReplacements(user, workspaceName, namespace);
+	String configMapYaml;
+	try {
+	    configMapYaml = ResourceUtil.readResourceAndReplacePlaceholders(DefaultWorkspaceAddedHandler.class,
+		    TEMPLATE_CONFIGMAP_YAML, replacements, correlationId);
+	} catch (IOException | URISyntaxException e) {
+	    LOGGER.error(formatLogMessage(correlationId, "Error while adjusting template"), e);
+	    return false;
+	}
+
+	try (ByteArrayInputStream inputStream = new ByteArrayInputStream(configMapYaml.getBytes())) {
+	    /* prepare new configmap */
+	    NonNamespaceOperation<ConfigMap, ConfigMapList, Resource<ConfigMap>> configMaps = client.configMaps()
+		    .inNamespace(namespace);
+	    LOGGER.trace(formatLogMessage(correlationId, "Loading new config map:\n" + configMapYaml));
+	    ConfigMap configMap = configMaps.load(inputStream).get();
+	    configMap.getMetadata().getOwnerReferences().get(0).setUid(workspaceResourceUID);
+	    configMap.getMetadata().getOwnerReferences().get(0).setName(workspaceResourceName);
+	    configMap.setData(data);
+
+	    /* apply new deployment */
+	    LOGGER.trace(formatLogMessage(correlationId, "Creating new config map"));
+	    configMaps.create(configMap);
+	    LOGGER.info(formatLogMessage(correlationId, "Created a new config map"));
+	} catch (IOException e) {
+	    LOGGER.error(formatLogMessage(correlationId, "Error with template stream"), e);
+	    return false;
+	}
+	return true;
+    }
+
+    protected Map<String, String> getConfigMapReplacements(String user, String workspaceName, String namespace) {
+	Map<String, String> replacements = new LinkedHashMap<String, String>();
+	replacements.put(PLACEHOLDER_CONFIGNAME, getConfigMapName(user, workspaceName));
+	replacements.put(PLACEHOLDER_NAMESPACE, namespace);
+	return replacements;
+    }
+
+    protected Map<String, String> getSidecarReplacements(String user, String workspaceName) {
+	Map<String, String> replacements = new LinkedHashMap<String, String>();
+	replacements.put(PLACEHOLDER_CONFIGNAME, getConfigMapName(user, workspaceName));
+	return replacements;
     }
 
     protected static boolean isUnusedService(Service service) {
