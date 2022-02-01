5f2a30d3206b563479dcf92db67d78329d463fa3
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2022-02-01 16:29:02 +0100

    Initial landing page
    * auth with keycloak and extract user mail
    * show a spinner
    
    Contributed on behalf of STMicroelectronics

---
+++ node/landing-page/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <img alt="Theia logo" src="./assets/logo.png" />
+  <WorkspaceLancher :email="email" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import WorkspaceLancher from "./components/WorkspaceLancher.vue";
+import Keycloak, { KeycloakConfig } from "keycloak-js";
+
+export default defineComponent({
+  name: "App",
+  props: {
+    keycloakAuthUrl: String,
+    keycloakRealm: String,
+    keycloakClientId: String,
+  },
+  components: {
+    WorkspaceLancher,
+  },
+  data() {
+    return {
+      email: undefined,
+    };
+  },
+  created() {
+    const keycloakConfig: KeycloakConfig = {
+      url: this.keycloakAuthUrl,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      realm: this.keycloakRealm!,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      clientId: this.keycloakClientId!,
+    };
+
+    const keycloak = Keycloak(keycloakConfig);
+
+    keycloak
+      .init({
+        onLoad: "login-required",
+        redirectUri: window.location.href,
+        checkLoginIframe: false,
+      })
+      .then((auth) => {
+        if (!auth) {
+          window.location.reload();
+        } else {
+          const parsedToken = keycloak.idTokenParsed;
+          if (parsedToken) {
+            this.email = parsedToken.email;
+          }
+        }
+      })
+      .catch(() => {
+        console.error("Authentication Failed");
+      });
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ node/landing-page/src/components/WorkspaceLancher.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>Hello {{ email }}. Please wait while we get your workspace ready.</div>
+  <div class="loader"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WorkspaceLancher",
+  props: {
+    email: String,
+  },
+});
+</script>
+
+<style scoped>
+.loader {
+  border: 16px solid #f3f3f3; /* Light grey */
+  border-top: 16px solid #0882ff; /* Blue */
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  margin-top: 25px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
