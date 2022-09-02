29220dbca87d362cae8f3b8aa46b46b426615695
Martin Fleck <mfleck@eclipsesource.com>
2022-09-02 12:02:12 +0200

    Make Quarkus more configurable
    - Upgrade to Quarkus 2.12 for additional properties
    - Increase default http read timeout to 5M
    - Allow customization of service port (default: 8081)
    - Print Quarkus configuration to debug on startup

---
+++ java/service/org.eclipse.theia.cloud.service/src/main/java/org/eclipse/theia/cloud/service/ApplicationLifecycleListener.java
@@ -0,0 +1,70 @@
+/********************************************************************************
+ * Copyright (C) 2022 EclipseSource and others.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is available at
+ * https://www.gnu.org/software/classpath/license.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ ********************************************************************************/
+package org.eclipse.theia.cloud.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.spi.ConfigSource;
+import org.jboss.logging.Logger;
+
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.core.json.JsonObject;
+
+@ApplicationScoped
+public class ApplicationLifecycleListener {
+    protected Logger logger;
+
+    protected void onStart(@Observes StartupEvent event) {
+	logger = Logger.getLogger(getClass());
+	logConfiguration();
+    }
+
+    private void logConfiguration() {
+	logConfigurationSources();
+	logQuarkusConfiguration();
+    }
+
+    private void logConfigurationSources() {
+	for (ConfigSource configSource : ConfigProvider.getConfig().getConfigSources()) {
+	    logger.info(configSource.getName() + " (" + configSource.getOrdinal() + ")");
+	    logger.info(new JsonObject(new HashMap<String, Object>(configSource.getProperties())).encodePrettily());
+	}
+    }
+
+    private void logQuarkusConfiguration() {
+	Config configuration = ConfigProvider.getConfig();
+	Map<String, Object> quarkusProperties = StreamSupport
+		.stream(configuration.getPropertyNames().spliterator(), false).filter(this::isQuarkusProperty) //
+		.collect(Collectors.toMap(Function.identity(),
+			property -> configuration.getValue(property, String.class)));
+	logger.info("Resulting Quarkus Configuration");
+	logger.info(new JsonObject(quarkusProperties).encodePrettily());
+    }
+
+    private boolean isQuarkusProperty(Object name) {
+	return name.toString().startsWith("quarkus.");
+    }
+
+}
