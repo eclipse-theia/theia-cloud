801fc51570e532ba070b0c5d6cd8fc6a2d323a03
Lucas Koehler <lkoehler@eclipsesource.com>
2023-02-08 16:45:29 +0100

    Add testing page to facilitate manual service tests
    - Simple web app based on CRA
    - Login and logout via keycloak
    - Requests for sessions and workspaces for same or other user
    
    Contributed on behalf of STMicroelectronics
    
    Signed-off-by: Lucas Koehler <lkoehler@eclipsesource.com>

---
+++ node/testing-page/README.md
@@ -0,0 +1,33 @@
+# TheiaCloud Testing App
+
+This folder contains a simple test app that allows to login via Keycloak and trigger various TheiaCloud endpoints (e.g. session start or stop).
+
+## Usage
+
+First, adapt the constants `KEYCLOAK_CONFIG` and `SERVICE_URL` at the top of [App.tsx](./src/App.tsx) to fit your deployment.
+
+Install dependencies and start app:
+
+```bash
+npm ci
+npm start
+```
+
+Open <http://localhost:3000>
+
+Click the Login button to login via Keycloak.
+Do not login to test anonymous requests.
+If the user input is filled, this is sent as the user in requests.
+If the input is empty, the logged in user is used.
+Specify a user different from the logged in one to test authorization of the service (i.e. that you cannot stop sessions of another user).
+
+Use your browser's DevTools to see outgoing requests and their results.
+Return values of outgoing requests are also logged to the console.
+
+## Tech Stack
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ node/testing-page/src/App.tsx
@@ -0,0 +1,169 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { KeycloakConfig } from 'keycloak-js';
+import Keycloak from 'keycloak-js';
+import { TheiaCloud, RequestOptions, SessionListRequest, SessionStartRequest, SessionStopRequest, WorkspaceCreationRequest, WorkspaceDeletionRequest, WorkspaceListRequest } from '@eclipse-theiacloud/common';
+
+const KEYCLOAK_CONFIG: KeycloakConfig = {
+  url: 'https://keycloak.localdemo.io/auth/',
+  realm: 'TheiaCloud',
+  clientId: 'theia-cloud'
+};
+const SERVICE_URL = 'https://localdemo.io/servicex';
+
+const APP_DEFINITION = 'theia-cloud-demo'
+const APP_ID = 'asdfghjkl';
+
+function App() {
+  const [token, setToken] = useState<string>();
+  const [logoutUrl, setLogoutUrl] = useState<string>()
+  const [email, setEmail] = useState<string>();
+  const [user, setUser] = useState('');
+  const [resourceName, setResourceName] = useState('');
+
+  useEffect(() => {
+    const keycloak = Keycloak(KEYCLOAK_CONFIG);
+    keycloak
+      .init({
+        onLoad: 'check-sso',
+        redirectUri: window.location.href,
+        checkLoginIframe: false,
+      })
+      .then(auth => {
+        if (auth) {
+          const parsedToken = keycloak.idTokenParsed;
+          if (parsedToken) {
+            const userMail = parsedToken.email;
+            setToken(keycloak.idToken);
+            setEmail(userMail);
+            setLogoutUrl(keycloak.createLogoutUrl());
+          }
+        }
+      })
+      .catch(() => {
+        console.error('Authentication Failed');
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  const login = () => {
+    const keycloak = Keycloak(KEYCLOAK_CONFIG);
+    keycloak
+      .init({
+        onLoad: 'login-required',
+        redirectUri: window.location.href,
+        checkLoginIframe: false
+      })
+      .catch(() => {
+        console.error('Authentication Failed');
+      });
+  }
+
+  const executeRequest = async (requestFn: RequestFunction): Promise<any> => {
+    if (!token) {
+      console.warn('No token. Request is anonymous.');
+    }
+    return requestFn(user ? user : email!, token!).then(val => {
+      console.log('Request successful with result:', val);
+    }).catch(err => {
+      console.error('Request failed:', err);
+    })
+  }
+
+  // Workspace requests
+  const listWorkspaces = (user: string, accessToken?: string) => {
+    const request: WorkspaceListRequest = {
+      appId: APP_ID,
+      user,
+      serviceUrl: SERVICE_URL
+    }
+    return TheiaCloud.Workspace.listWorkspaces(request, generateRequestOptions(accessToken));
+  }
+  const createWorkspace = (user: string, accessToken?: string) => {
+    const request: WorkspaceCreationRequest = {
+      appId: APP_ID,
+      appDefinition: APP_DEFINITION,
+      user,
+      serviceUrl: SERVICE_URL
+    }
+    return TheiaCloud.Workspace.createWorkspace(request, generateRequestOptions(accessToken));
+  }
+  const deleteWorkspace = (user: string, accessToken?: string) => {
+    const request: WorkspaceDeletionRequest = {
+      appId: APP_ID,
+      user,
+      workspaceName: resourceName,
+      serviceUrl: SERVICE_URL
+    }
+    return TheiaCloud.Workspace.deleteWorkspace(request, generateRequestOptions(accessToken));
+  }
+  
+  // Session requests
+  const listSessions = (user: string, accessToken?: string) => {
+    const request: SessionListRequest = {
+      appId: APP_ID,
+      user,
+      serviceUrl: SERVICE_URL
+    }
+    return TheiaCloud.Session.listSessions(request, generateRequestOptions(accessToken));
+  }
+  const startSession = (user: string, accessToken?: string) => {
+    const request: SessionStartRequest = {
+      appId: APP_ID,
+      appDefinition: APP_DEFINITION,
+      user,
+      workspaceName: resourceName ? resourceName : undefined,
+      serviceUrl: SERVICE_URL
+    }
+    return TheiaCloud.Session.startSession(request, generateRequestOptions(accessToken));
+  }
+  const stopSession = (user: string, accessToken?: string) => {
+    const request: SessionStopRequest = {
+      appId: APP_ID,
+      user,
+      sessionName: resourceName,
+      serviceUrl: SERVICE_URL
+    }
+    return TheiaCloud.Session.stopSession(request, generateRequestOptions(accessToken));
+  }
+
+  return (
+    <div className="App">
+      <h1>TheiaCloud Service Test Page</h1>
+      <p><strong>This page is meant for internal testing only!</strong></p>
+      <p>Open your browser's dev tools (F12) to see outgoing requests. Results are logged to the console as well.</p>
+      {email ? <p>Logged in as: {email}</p> : <button onClick={login}>Login via Keycloak</button>}
+      {logoutUrl && <a href={logoutUrl}>Logout</a>}
+      <p>AppId: {APP_ID}</p>
+      <p>Service URL: {SERVICE_URL}</p>
+      <p>
+        <span>User:</span>
+        <input type='text' value={user} onChange={ev => setUser(ev.target.value)} />
+      </p>
+      <p>
+        <span>Session/Workspace:</span>
+        <input type='text' value={resourceName} onChange={ev => setResourceName(ev.target.value)} />
+      </p>
+      <p>
+        <button onClick={() => executeRequest(listSessions)}>List Sessions</button>
+        <button onClick={() => executeRequest(startSession)}>Start Session</button>
+        <button onClick={() => executeRequest(stopSession)}>Stop Session</button>
+      </p>
+      <p>
+        <button onClick={() => executeRequest(listWorkspaces)}>List Workspaces</button>
+        <button onClick={() => executeRequest(createWorkspace)}>Create Workspace</button>
+        <button onClick={() => executeRequest(deleteWorkspace)}>Delete Workspace</button>
+      </p>
+    </div>
+  );
+};
+
+type RequestFunction = (user: string, token: string | undefined) => Promise<any>;
+
+function generateRequestOptions(token: string | undefined): RequestOptions {
+  return {
+    accessToken: token,
+  };
+}
+export default App;
