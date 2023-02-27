ff72b6fdd987dc5288c2c93b819e4c962ff7b053
Simon Graband <sgraband@eclipsesource.com>
2023-02-27 10:00:21 +0100

    Add authentication to the try now page
    Moved components into separate files.
    Added header that contains login/logout buttons.
    User email will be displayed if logged in.
    Added hint for why the login is needed.
    
    Contributed on behalf of STMicroelectronics

--- node/try-now-page/src/App.tsx
+++ node/try-now-page/src/App.tsx
@@ -1,15 +1,22 @@
+/* eslint-disable max-len */
 import './App.css';
 
 import { AppDefinition, getTheiaCloudConfig, LaunchRequest, PingRequest, TheiaCloud } from '@eclipse-theiacloud/common';
-import React, { useEffect, useState } from 'react';
+import Keycloak, { KeycloakConfig } from 'keycloak-js';
+import { useEffect, useState } from 'react';
 
-import { AppLogo } from './components/AppLogo';
-import { Spinner } from './components/Spinner';
+import { ErrorComponent } from './components/ErrorComponent';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { LaunchApp } from './components/LaunchApp';
+import { Loading } from './components/Loading';
+import { LoginInfo } from './components/LoginInfo';
 
 // global state to be kept between render calls
-let initialised = false;
+let initialized = false;
 let initialAppName = '';
 let initialAppDefinition = '';
+let keycloakConfig: KeycloakConfig | undefined = undefined;
 
 function App(): JSX.Element {
   const [config] = useState(() => getTheiaCloudConfig());
@@ -24,7 +31,7 @@ function App(): JSX.Element {
     );
   }
 
-  if (!initialised) {
+  if (!initialized) {
     initialAppName = config.appName;
     initialAppDefinition = config.appDefinition;
   }
@@ -32,19 +39,29 @@ function App(): JSX.Element {
   const [selectedAppName, setSelectedAppName] = useState(initialAppName);
   const [selectedAppDefinition, setSelectedAppDefinition] = useState(initialAppDefinition);
 
+  const [email, setEmail] = useState<string>();
+  const [token, setToken] = useState<string>();
+  const [logoutUrl, setLogoutUrl] = useState<string>();
+  const [showLoginInformation, setShowLoginInformation] = useState<boolean>(false);
+
   useEffect(() => {
-    if (!initialised) {
-      initialised = true;
+    if (!initialized) {
+      initialized = true;
       const element = document.getElementById('selectapp');
       const urlParams = new URLSearchParams(window.location.search);
       if (urlParams.has('appDef') || urlParams.has('appdef')) {
         const defaultSelection = urlParams.get('appDef') || urlParams.get('appdef');
-        // eslint-disable-next-line no-null/no-null
-        if (defaultSelection !== null && isDefaultSelectionValueValid(defaultSelection, config.appDefinition, config.additionalApps)) {
+        if (
+          // eslint-disable-next-line no-null/no-null
+          defaultSelection !== null &&
+          isDefaultSelectionValueValid(defaultSelection, config.appDefinition, config.additionalApps)
+        ) {
           // eslint-disable-next-line no-null/no-null
           if (element !== null && config.additionalApps && config.additionalApps.length > 0) {
             (element as HTMLSelectElement).value = defaultSelection;
-            setSelectedAppName((element as HTMLSelectElement).options[(element as HTMLSelectElement).selectedIndex].text);
+            setSelectedAppName(
+              (element as HTMLSelectElement).options[(element as HTMLSelectElement).selectedIndex].text
+            );
             setSelectedAppDefinition((element as HTMLSelectElement).value);
           } else {
             // If there are no additional apps, just use the application id as the name
@@ -56,10 +73,71 @@ function App(): JSX.Element {
           console.log('Set ' + defaultSelection + ' as default selection');
         }
       }
+      if (config.useKeycloak) {
+        keycloakConfig = {
+          url: config.keycloakAuthUrl,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          realm: config.keycloakRealm!,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          clientId: config.keycloakClientId!
+        };
+        const keycloak = Keycloak(keycloakConfig);
+        keycloak
+          .init({
+            onLoad: 'check-sso',
+            redirectUri: window.location.href,
+            checkLoginIframe: false
+          })
+          .then(auth => {
+            if (auth) {
+              const parsedToken = keycloak.idTokenParsed;
+              if (parsedToken) {
+                const userMail = parsedToken.email;
+                setToken(keycloak.idToken);
+                setEmail(userMail);
+                setLogoutUrl(keycloak.createLogoutUrl());
+              }
+            }
+          })
+          .catch(() => {
+            console.error('Authentication Failed');
+          });
+      }
     }
-  },[]);
+  }, []);
+
+  useEffect(() => {
+    token ? setShowLoginInformation(false) : setShowLoginInformation(true);
+  }, [token]);
 
   document.title = `${selectedAppName} - Try Now`;
+
+  const authenticate = (): void => {
+    const keycloak = Keycloak(keycloakConfig);
+    keycloak
+      .init({
+        onLoad: 'login-required',
+        redirectUri: window.location.href,
+        checkLoginIframe: false
+      })
+      .then((auth: any) => {
+        if (!auth) {
+          window.location.reload();
+        } else {
+          const parsedToken = keycloak.idTokenParsed;
+          if (parsedToken) {
+            const userMail = parsedToken.email;
+            setToken(keycloak.idToken);
+            setEmail(userMail);
+            setLogoutUrl(keycloak.createLogoutUrl());
+          }
+        }
+      })
+      .catch(() => {
+        console.error('Authentication Failed');
+        setError('Authentication failed');
+      });
+  };
 
   const handleStartSession = (appDefinition: string): void => {
     console.log('Launching ' + appDefinition);
@@ -70,139 +148,79 @@ function App(): JSX.Element {
     TheiaCloud.ping(PingRequest.create(config.serviceUrl, config.appId))
       .then(() => {
         // ping successful continue with launch
-        TheiaCloud.launchAndRedirect(config.useEphemeralStorage
-          ? LaunchRequest.ephemeral(config.serviceUrl, config.appId, appDefinition, 5)
-          : LaunchRequest.createWorkspace(config.serviceUrl, config.appId, appDefinition, 5),
-        { timeout: 60000, retries: 5 }
+        const workspace = config.useEphemeralStorage
+          ? undefined
+          : 'ws-' + config.appId + '-' + selectedAppDefinition + '-' + email;
+        TheiaCloud.launchAndRedirect(
+          config.useEphemeralStorage
+            ? LaunchRequest.ephemeral(config.serviceUrl, config.appId, appDefinition, 5, email)
+            : LaunchRequest.createWorkspace(config.serviceUrl, config.appId, appDefinition, 5, email, workspace),
+          { timeout: 60000, retries: 5, accessToken: token }
         )
           .catch((err: Error) => {
             if (err && (err as any).status === 473) {
-              setError(`The app definition '${appDefinition}' is not available in the cluster.\n` +
-                'Please try launching another application.');
+              setError(
+                `The app definition '${appDefinition}' is not available in the cluster.\n` +
+                  'Please try launching another application.'
+              );
               return;
-            };
-            setError('Sorry, there are no more available instances in the cluster.\n'
-          + 'Please try again later, instances are shut down after 30 minutes.\n\n'
-          + 'Note: this is not a technical limit of Theia.cloud, but was intentionally set by us to keep this free offer within its intended budget.');
+            }
+            setError(err.message);
           })
           .finally(() => {
             setLoading(false);
           });
       })
       .catch((_err: Error) => {
-        setError('Sorry, we are performing some maintenance at the moment.\n'
-        + 'Please try again later. Usually maintenance won\'t last longer than 60 minutes.\n\n');
+        setError(
+          'Sorry, we are performing some maintenance at the moment.\n' +
+            "Please try again later. Usually maintenance won't last longer than 60 minutes.\n\n"
+        );
         setLoading(false);
       });
   };
 
   return (
     <div className='App'>
-      {loading ? <Loading /> :
-        <LaunchApp appName={selectedAppName} appDefinition={selectedAppDefinition} onStartSession={handleStartSession} />}
-      <ErrorComponent message={error} />
-      <div className='App__footer'>
-        { config.additionalApps && config.additionalApps.length > 0 &&
-          <p>
-            <label htmlFor="selectapp">Select app to launch </label>
-            <select name="apps" id="selectapp" onChange={event => {
-              setSelectedAppName(event.target.options[event.target.selectedIndex].text);
-              setSelectedAppDefinition(event.target.value);
-            }}>
-              <option value={config.appDefinition}>{config.appName}</option>
-              { config.additionalApps.map((app, index) => <option key={index + 1} value={app.appId}>{app.appName}</option>)}
-            </select>
-          </p>
+      {config.useKeycloak ? (
+        <Header email={email} authenticate={authenticate} logoutUrl={logoutUrl} />
+      ) : (
+        <div className='header'></div>
+      )}
+      <div className='body'>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <LaunchApp
+              appName={selectedAppName}
+              appDefinition={selectedAppDefinition}
+              onStartSession={handleStartSession}
+              token={token}
+              config={config}
+            />
+          </div>
+        )}
+        <ErrorComponent message={error} />
+        {
+          // eslint-disable-next-line no-null/no-null
+          config.useKeycloak ? <LoginInfo showLoginInformation={showLoginInformation} /> : null
         }
-        <p>
-          Powered by{' '}
-          <a href='http://theia-cloud.io' target='_blank' rel='noreferrer'>
-            Theia.Cloud
-          </a>
-        </p>
-        <p>
-          Having problems? Please{' '}
-          <a target='_blank' href='https://github.com/eclipsesource/theia-cloud/issues' rel='noreferrer'>
-            report an issue
-          </a>
-          .
-        </p>
+        <Footer
+          config={config}
+          setSelectedAppName={setSelectedAppName}
+          setSelectedAppDefinition={setSelectedAppDefinition}
+        />
       </div>
     </div>
   );
 }
 
-const Loading = (): JSX.Element => <div>
-  <Spinner />
-  <p className='Loading__description'>
-      We will now spawn a dedicated Blueprint for you, hang in tight, this might take up to 3 minutes.
-  </p>
-</div>;
-
-interface LaunchAppProps {
-  appName: string;
-  appDefinition: string;
-  onStartSession: (appDefinition: string) => void;
-}
-const LaunchApp: React.FC<LaunchAppProps> = ({ appName, appDefinition, onStartSession }) => {
-  const [acceptedTerms, setAcceptedTerms] = useState(false);
-  return (
-    <div>
-      <AppLogo />
-      <p>
-        <button
-          className='App__try-now-button'
-          disabled={!acceptedTerms}
-          onClick={() => onStartSession(appDefinition)} >
-          Launch {appName} &nbsp;&nbsp;&rarr;
-        </button>
-      </p>
-      <TermsAndConditions accepted={acceptedTerms} onTermsAccepted={setAcceptedTerms} />
-    </div>
-  );
-};
-
-interface ErrorComponentProps {
-  message?: string;
-}
-
-const ErrorComponent: React.FC<ErrorComponentProps> = ({ message }) => message !== undefined ? (
-  <div className='App__error-message'>
-    <h2>
-      <strong>
-          Oh no, something went wrong! &#9785;
-      </strong>
-    </h2>
-    <pre>ERROR: {message}</pre>
-  </div>
-// eslint-disable-next-line no-null/no-null
-) : null;
-interface TermsAndConditionsProps {
-  accepted: boolean;
-  onTermsAccepted: (accepted: boolean) => void;
-}
-const openTerms = (): boolean => {
-  window.open('./terms.html', 'popup', 'width=600,height=600');
-  return false;
-};
-const TermsAndConditions: React.FC<TermsAndConditionsProps> = ({ accepted, onTermsAccepted }) => (
-  <div className='terms-and-conditions'>
-    <input
-      id='accept-terms'
-      type='checkbox'
-      checked={accepted}
-      onChange={ev => onTermsAccepted(ev.target.checked)}
-    />
-    <label htmlFor='accept-terms'>
-        I accept the{' '}
-      <a target='popup' href='./terms.html' onClick={openTerms}>
-          terms and conditions.
-      </a>
-    </label>
-  </div>
-);
-
-function isDefaultSelectionValueValid(defaultSelection: string, appDefinition: string, additionalApps?: AppDefinition[]): boolean {
+function isDefaultSelectionValueValid(
+  defaultSelection: string,
+  appDefinition: string,
+  additionalApps?: AppDefinition[]
+): boolean {
   if (defaultSelection === appDefinition) {
     return true;
   }
@@ -214,4 +232,3 @@ function isDefaultSelectionValueValid(defaultSelection: string, appDefinition: s
 }
 
 export default App;
-
---
+++ node/try-now-page/src/components/ErrorComponent.tsx
@@ -0,0 +1,14 @@
+interface ErrorComponentProps {
+  message?: string;
+}
+
+export const ErrorComponent: React.FC<ErrorComponentProps> = ({ message }: ErrorComponentProps) =>
+  message !== undefined ? (
+    <div className='App__error-message'>
+      <h2>
+        <strong>Oh no, something went wrong! &#9785;</strong>
+      </h2>
+      <pre>ERROR: {message}</pre>
+    </div>
+  ) : // eslint-disable-next-line no-null/no-null
+  null;
