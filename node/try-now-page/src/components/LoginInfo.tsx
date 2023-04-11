2963968f42d262c3da83f522a31ff2c0a99900a8
Johannes Faltermeier <jfaltermeier@eclipsesource.com>
2023-04-11 13:04:00 +0200

    trynow: Update wording regarding login

--- node/try-now-page/src/components/LoginInfo.tsx
+++ node/try-now-page/src/components/LoginInfo.tsx
@@ -3,12 +3,11 @@ export const LoginInfo: React.FC = () => (
     <h2>
       <strong>Please login to use this demo</strong>
     </h2>
-    <pre>
-        We only need your login data for technical reasons (e.g. avoiding crypto-mining or other harmful actions).
-    </pre>
-    <pre>We will not contact you via your e-mail or use it for any other marketing purposes.</pre>{' '}
-    <pre>
-        You can login using your Github account (You will be forwarded to our Keycloak instance).
-    </pre>
+    <p>We only need your login data for preventing misuse.</p>
+    <p>
+      We <strong>never</strong> contact you via your e-mail and will <strong>never</strong> use it for any other
+      marketing purposes.
+    </p>{' '}
+    <p>You can use your Github account to login after clicking the login button.</p>
   </div>
 );
