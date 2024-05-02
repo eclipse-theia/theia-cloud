605f2f47c163452e657a60c8669041a77dd39b75
Lucas Koehler <lkoehler@eclipsesource.com>
2024-05-02 17:11:51 +0200

    Add ViteJS based landing page based on the try now page
    - Remove vue based simple landing page
    - Migrate Try Now page from CRA to ViteJS and add as new `landing-page`
    - Increase Keycloak dependency from 20.0.3 to 20.0.5
    - The new landing-page uses its own tsconfig.json based on the one generated by Vite
    - Adapt landing page dockerfile to new vite-based page
    - Remove terms and conditions
    - misc: Update node VSCode settings
    
    Contributed on behalf of STMicroelectronics

---
+++ node/landing-page/src/components/Footer.tsx
@@ -0,0 +1,47 @@
+import { AppDefinition } from '@eclipse-theiacloud/common';
+
+interface FooterProps {
+  appDefinition: string,
+  appName: string,
+  additionalApps: AppDefinition[],
+  setSelectedAppName: (value: React.SetStateAction<string>) => void,
+  setSelectedAppDefinition: (value: React.SetStateAction<string>) => void
+}
+
+export const Footer = ({appDefinition, appName, additionalApps, setSelectedAppName, setSelectedAppDefinition}: FooterProps): JSX.Element => (
+  <div className='App__footer'>
+    {additionalApps.length > 0 && (
+      <p>
+        <label htmlFor='selectapp'>Select app to launch </label>
+        <select
+          name='apps'
+          id='selectapp'
+          onChange={event => {
+            setSelectedAppName(event.target.options[event.target.selectedIndex].text);
+            setSelectedAppDefinition(event.target.value);
+          }}
+        >
+          <option value={appDefinition}>{appName}</option>
+          {additionalApps.map((app: any, index: number) => (
+            <option key={index + 1} value={app.appId}>
+              {app.appName}
+            </option>
+          ))}
+        </select>
+      </p>
+    )}
+    <p>
+      Powered by{' '}
+      <a href='http://theia-cloud.io' target='_blank' rel='noreferrer'>
+        Theia Cloud
+      </a>
+    </p>
+    <p>
+      Having problems? Please{' '}
+      <a target='_blank' href='https://github.com/eclipsesource/theia-cloud/issues' rel='noreferrer'>
+        report an issue
+      </a>
+      .
+    </p>
+  </div>
+);
