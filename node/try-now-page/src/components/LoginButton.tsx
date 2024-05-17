21050f3563b748306b299f588303971196f3d6f0
Lucas Koehler <lkoehler@eclipsesource.com>
2024-05-17 16:45:18 +0200

    Remove try now page
    - Remove source code
    - Remove Dockerfile and CI
    - Regenerate package-lock.json to remove try now workspace
    - Adapt terraform values to use the landing page image
    
    Contributed on behalf of STMicroelectronics

--- node/try-now-page/src/components/LoginButton.tsx
+++
@@ -1,9 +0,0 @@
-export interface LoginButtonProps {
-  login: () => void;
-}
-export const LoginButton: React.FC<LoginButtonProps> = ({login}: LoginButtonProps) => (<button
-  className='App__try-now-button'
-  onClick={() => login()}
->
-    Login
-</button>);
