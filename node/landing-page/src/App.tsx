a95dc0ae11e4a493b54681e2e8236e24d6e58a86
Yannik Schmidt <yannik.schmidt@tum.de>
2024-11-05 16:28:28 +0100

    Add debug to keycloak

--- node/landing-page/src/App.tsx
+++ node/landing-page/src/App.tsx
@@ -162,7 +162,15 @@ function App(): JSX.Element {
       return;
     }
 
-    if (selectedAppDefinition  && gitUri && artemisToken && !loading && (config.useKeycloak ? token : true)) {
+    console.log(`Keycloak required: ${config.useKeycloak}`);
+    if (token) {
+      console.log('Token okay');
+    } else {
+      console.log('Token missing');
+    }
+    console.log(`=> KeyCloak success: ${Boolean(config.useKeycloak ? token : true)}`)
+
+    if (selectedAppDefinition && gitUri && artemisToken && !loading && (config.useKeycloak ? token : true)) {
       console.log('Checking auth, setting autoStart to true and starting session');
       //authenticate();
       setAutoStart(true);
