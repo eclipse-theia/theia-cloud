4911016b9bcd2d10be0e3130b30ce9951e873cc1
Lukas Kratzel <lukaskratzel2004@gmail.com>
2025-11-17 15:36:46 +0100

    Add session startup time metrics to theia service

--- java/service/org.eclipse.theia.cloud.service/src/main/java/org/eclipse/theia/cloud/service/session/SessionResource.java
+++ java/service/org.eclipse.theia.cloud.service/src/main/java/org/eclipse/theia/cloud/service/session/SessionResource.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.eclipse.theia.cloud.common.k8s.resource.session.SessionSpec;
 import org.eclipse.theia.cloud.common.k8s.resource.workspace.Workspace;
 import org.eclipse.theia.cloud.common.util.TheiaCloudError;
@@ -65,6 +66,7 @@ public class SessionResource extends BaseResource {
     }
 
     @Operation(summary = "Start a new session", description = "Starts a new session for an existing workspace and responds with the URL of the started session.")
+    @Timed(name = "theiacloud_session_startup_seconds", description = "Time taken from REST request to session URL being available.", absolute = true)
     @POST
     @NoAnonymousAccess
     public String start(SessionStartRequest request) {
