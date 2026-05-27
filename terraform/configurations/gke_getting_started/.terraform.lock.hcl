a6420765c93d1f69774d13197634f55043149145
Lucas Koehler <lkoehler@eclipsesource.com>
2026-05-27 15:59:51 +0200

    terraform: adapt gke getting started to use cluster-prerequisites
    - Remove usage of helm module
    - Install cert-manager and theia-cloud-base before cluster-prerequisites
      because we re-use theia cloud's let's encrypt cluster issuer

--- terraform/configurations/gke_getting_started/.terraform.lock.hcl
+++ terraform/configurations/gke_getting_started/.terraform.lock.hcl
@@ -46,7 +46,7 @@ provider "registry.terraform.io/hashicorp/google" {
 
 provider "registry.terraform.io/hashicorp/helm" {
   version     = "3.0.2"
-  constraints = ">= 3.0.2"
+  constraints = ">= 2.0.0, >= 3.0.2"
   hashes = [
     "h1:+tHGl509bhyUrvvj9GQTBsdK+ImHJnRuo6ppDZPavqY=",
     "zh:2778de76c7dfb2e85c75fe6de3c11172a25551ed499bfb9e9f940a5be81167b0",
@@ -61,6 +61,47 @@ provider "registry.terraform.io/hashicorp/helm" {
     "zh:e583b2b478dde67da28e605ab4ef6521c2e390299b471d7d8ef05a0b608dcdad",
     "zh:f238b86611647c108c073d265f8891a2738d3158c247468ae0ff5b1a3ac4122a",
     "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/http" {
+  version     = "3.6.0"
+  constraints = ">= 3.0.0"
+  hashes = [
+    "h1:FekY+4cjIw3QBdpk2dVQ20+t8AeDwCK/VaKOnjfeJvw=",
+    "zh:0996c7db5d7627bc6ab8c4d217f18fb122d60e99e454812b080ee5695cad1003",
+    "zh:25b7ee0ba9edc912a00365c776d062ae7c66d94050c6c13038447c8e8b95ddf2",
+    "zh:29c4ba54add6eee7f1d0034d331ba0f14f3046234b1f7520a537e6444e4521b7",
+    "zh:30a3aa3ef978f8142daf2ced3f9f1ecda8b0831cdc6911e7e930e95eab191b4f",
+    "zh:30bf0810acdfe96e799ed9b64cb70e96d6f7c033621e0373b9897513977c49c5",
+    "zh:4ecaaf3dfd20feaa2b92af521018501bc7d874b6a6642ef86ea4cc3c251d737a",
+    "zh:661760406e3d5372e6725d18ca80734996f21adabc02d82e36ed6d8db07cad7d",
+    "zh:70dd9556bd2633082efd9681421f89dd4abdde6fcd84834627fa4b8b8f9e7afe",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:973a72066866579ac6fa0e0c9e9eee8e77d238cf78f1b1f96ce6536052fa73ce",
+    "zh:b2ac9463a9499b478147186027fabee65c04b8f6d963a8df3d49241ce5784fe2",
+    "zh:c6acc05dc3456c0001b5fb99e1b57005f5a3d3d766f9bddfc21ffe8364fa7535",
+    "zh:f97f2d57ebcefe62f0644f58e8ba68593f8f05baf7856c99953396aedd14e415",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "3.1.0"
+  constraints = ">= 2.0.0"
+  hashes = [
+    "h1:oodIAuFMikXNmEtil5MQgP4dfSctUBYQiGJfjbsF3NY=",
+    "zh:0215c5c60be62028c09a2f22458e89cda3ef5830a632299f1d401eb3538874b0",
+    "zh:09ebb9f442431e278a310a9423f32caf467cb4b3cad3fe59573ca71fa7b14e20",
+    "zh:0c4e5912f83bb35846ae0a9ae54fc320706ee61894cd21cc6b4181b1c5a2fa5c",
+    "zh:1678c982853ad461e65ccb5e79d585e13ed109dd47dab2a66d3a7a304faeef65",
+    "zh:1c050a5c15e330457a9c18caacf61a923c59d663e13f2962e4b32f04fef523a0",
+    "zh:2c55bcec83be58ec132c7cb0a1ac644758b800d794fdc636d53a0eada0358a3a",
+    "zh:a062bb0aa316c08d8460c66a5d68da71da40de5d3bc3b31abcf3a1a9a19650f1",
+    "zh:a26fdea0afaa9b247c73c0b42843ca51ba7db0ac2571f9d3d50dcabd20ca1b98",
+    "zh:c872c9385a78d502bf5823d61cd3bb0f9a0585030e025eb12585c83451beeaa1",
+    "zh:f180879af931182beee4c8c0d9dab62b81d86f17ddcbe3786ef4c7cec9163a4e",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:f70f5789264069e0eef06f9b5d5fde955ef7206f7d446d1ce51a4c37a3f3e02f",
   ]
 }
 
--- terraform/configurations/gke_getting_started/gke_getting_started.tf
+++ terraform/configurations/gke_getting_started/gke_getting_started.tf
@@ -66,30 +66,63 @@ provider "kubectl" {
   cluster_ca_certificate = module.cluster.cluster_ca_certificate
 }
 
+# Manually install cert-manager to have its CRDs available for the theia-cloud-base module, which installs the cluster issuer for let's encrypt.
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  version          = "v1.17.4"
+  namespace        = "cert-manager"
+  create_namespace = true
+
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+  ]
+}
+
+# Install theia-cloud-base first because we re-use the "letsencrypt-prod" ClusterIssuer created by that module for the Keycloak ingress installed in the cluster prerequisites module.
+module "theia-cloud-base" {
+  source = "../../modules/theia-cloud"
+
+  depends_on = [helm_release.cert_manager]
+
+  install_theia_cloud_crds  = false
+  install_theia_cloud       = false
+  hostname                  = "${google_compute_address.host_ip.address}.sslip.io"
+  cert_manager_issuer_email = var.cert_manager_issuer_email
+}
+
 module "cluster_prerequisites" {
   source = "../../modules/cluster-prerequisites"
 
+  depends_on                          = [module.theia-cloud-base]
   hostname                            = "${google_compute_address.host_ip.address}.sslip.io"
   keycloak_admin_password             = var.keycloak_admin_password
   postgres_password                   = var.postgres_password
-  install_cert_manager                = true
+  install_cert_manager                = false
   install_ingress_controller          = true
   install_selfsigned_issuer           = false
   cert_manager_issuer_email           = var.cert_manager_issuer_email
+  ingress_controller_type             = var.ingress_controller_type
+  ingress_class_name                  = var.ingress_controller_type
   ingress_cert_manager_cluster_issuer = "letsencrypt-prod"
   load_balancer_ip                    = google_compute_address.host_ip.address
 }
 
-module "helm" {
-  source = "../../modules/helm"
+module "theia-cloud" {
+  source = "../../modules/theia-cloud"
 
-  depends_on                  = [module.cluster_prerequisites]
-  ingress_controller_type     = var.ingress_controller_type
-  cert_manager_issuer_email   = var.cert_manager_issuer_email
-  cert_manager_cluster_issuer = "letsencrypt-prod"
-  cert_manager_common_name    = "${google_compute_address.host_ip.address}.sslip.io"
-  hostname                    = "${google_compute_address.host_ip.address}.sslip.io"
-  loadBalancerIP              = google_compute_address.host_ip.address
+  depends_on = [module.cluster_prerequisites]
+
+  install_theia_cloud_base  = false
+  hostname                  = "${google_compute_address.host_ip.address}.sslip.io"
+  ingress_controller_type   = var.ingress_controller_type
+  cert_manager_issuer_email = var.cert_manager_issuer_email
+  cloud_provider            = "K8S"
+  keycloak_url              = module.cluster_prerequisites.keycloak_url
 }
 
 provider "keycloak" {
